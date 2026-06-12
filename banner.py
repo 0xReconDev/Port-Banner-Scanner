2cb47818a4bbaac8ecb75ac81650b1471101e852
0xReconDev <forgamestismai@gmail.com>
2026-06-12 09:12:22 -0300

    Update banner.py

--- banner.py
+++ banner.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+import requests
 
 ip = sys.argv[1]
 ports = [21, 22, 80, 443]
@@ -11,20 +12,12 @@ for port in ports:
 		code = sock.connect_ex((ip, port))
 
 		if code == 0:
-
-			if port == 443:
-				try:
-					requisicao_https = requests.head(f"https://{ip}/", timeout=3)
-					print (f"[+] {port} - HTTPS HEAD status: {requisicao_https.status_code}.")
-				except requests.RequestException as e:
-					print (f"{port} - Erro na requisição HTTPS.")
-			elif port == 80:
-				try:
-					# Tentar na porta 80
-					requisicao = requests.head(f"http://{ip}/", timeout=3)
-					print (f"[+] {port} - HTTP HEAD status: {requisicao.status_code}")
-				except requests.RequestException as e:
-					print (f"{port} - Erro na requisição HTTP: {e}")
+			try:
+				requisicao_https = requests.head(f"https://{ip}/", timeout=3)
+				print (f"[+] {port} - HTTPS HEAD status: {requisicao_https.status_code}.")
+			except requests.RequestException as e:
+				print (f"{port} - Erro na requisição HTTPS.")
+				
 		sock.close()
 	except Exception as e:
 		print (f"Erro ao conectar na porta {port}: {e}")
